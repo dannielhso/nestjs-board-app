38e04c34ab8ab4a970e9e1711fec0cce26d51c2a
dannielhso <dannielha@naver.com>
2025-01-21 12:36:34 +0900

    feat : 게시글 조회 기능 구현
    - 컨트롤러 작성
    - 서비스 작성 (메모리 데이터베이스 사용상태)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get,  } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.entity';
 
 @Controller('boards') // 컨트롤러의 end 포인트 지정.
 export class BoardsController {
+    // 생성자 주입
     constructor(private boardsService :BoardsService) {}
 
-    @Get('hello')
-    async getHello(): Promise<string> {
-	    return this.boardsService.hello();
+    // 게시글 조회 기능
+    @Get('/')
+    getAllBoards(): Board[] {
+	    return this.boardsService.getAllBoards();
     }
 }
