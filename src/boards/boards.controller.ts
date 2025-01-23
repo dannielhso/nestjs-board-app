10dc5490fa3a2abdfcaa65a66b064b234ff3f71c
dannielhso <dannielha@naver.com>
2025-01-23 15:47:49 +0900

    refactor : Controller 계층의 async/await 비동기 Promise 반환으로 변경.

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -13,50 +13,50 @@ export class BoardsController {
 
     // 게시글 조회 기능
     @Get('/')
-    getAllBoards(): Board[] {
+    async getAllBoards(): Promise<Board[]> {
 	    return this.boardsService.getAllBoards();
     }
 
-    // 특정 게시글 조회 기능
-    @Get('/:id')
-    @UsePipes(ValidationPipe)
-    getBoardDetailById(@Param('id', ParseIntPipe) id:number): Board {
-	    return this.boardsService.getBoardDetailById(id);
-    }
+//     // 특정 게시글 조회 기능
+//     @Get('/:id')
+//     @UsePipes(ValidationPipe)
+//     getBoardDetailById(@Param('id', ParseIntPipe) id:number): Board {
+// 	    return this.boardsService.getBoardDetailById(id);
+//     }
 
-    // 키워드(작성자)로 검색한 게시글 조회 기능
-    @Get('/search/:keyword')
-    getBoardsByKeyword(@Query('author') author:string): Board[] {
-	    return this.boardsService.getBoardsByKeyword(author);
-    }
+//     // 키워드(작성자)로 검색한 게시글 조회 기능
+//     @Get('/search/:keyword')
+//     getBoardsByKeyword(@Query('author') author:string): Board[] {
+// 	    return this.boardsService.getBoardsByKeyword(author);
+//     }
 
-    // 게시글 작성 기능
-    @Post('/')
-    @UsePipes(ValidationPipe)
-    createBoard(@Body() createBoardDto: CreateBoardDto) {
-        return this.boardsService.createBoard(createBoardDto);
-    }
+//     // 게시글 작성 기능
+//     @Post('/')
+//     @UsePipes(ValidationPipe)
+//     createBoard(@Body() createBoardDto: CreateBoardDto) {
+//         return this.boardsService.createBoard(createBoardDto);
+//     }
 
-    // 특정 번호의 게시글 수정
-    @Put('/:id')
-    @UsePipes(ValidationPipe)
-    updateBoardById(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() updateBoardDto: UpdateBoardDto) {
-            return this.boardsService.updateBoardById(id, updateBoardDto)
-        }
+//     // 특정 번호의 게시글 수정
+//     @Put('/:id')
+//     @UsePipes(ValidationPipe)
+//     updateBoardById(
+//         @Param('id', ParseIntPipe) id: number,
+//         @Body() updateBoardDto: UpdateBoardDto) {
+//             return this.boardsService.updateBoardById(id, updateBoardDto)
+//         }
 
-    // 특정 게시글 일부 수정 기능 PATCH
-    @Patch('/:id')
-    updateBoardStatusById(
-        @Param('id', ParseIntPipe) id: number,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board{
-        return this.boardsService.updateBoardStatusById(id, status);
-    }
+//     // 특정 게시글 일부 수정 기능 PATCH
+//     @Patch('/:id')
+//     updateBoardStatusById(
+//         @Param('id', ParseIntPipe) id: number,
+//         @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board{
+//         return this.boardsService.updateBoardStatusById(id, status);
+//     }
 
-    // 게시글 삭제 기능
-    @Delete('/:id')
-    deleteBoardById(@Param('id', ParseIntPipe) id: number): void {
-        this.boardsService.deleteBoardById(id);
-    }
+//     // 게시글 삭제 기능
+//     @Delete('/:id')
+//     deleteBoardById(@Param('id', ParseIntPipe) id: number): void {
+//         this.boardsService.deleteBoardById(id);
+//     }
 }
